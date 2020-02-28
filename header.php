183e07989e761c6e829a0f4f28b80c6d59ccd802
AnnaDorogaya <43620643+AnnaDorogaya@users.noreply.github.com>
2020-02-27 22:13:59 -0800

    Change

--- header.php
+++ header.php
@@ -1,14 +1,28 @@
+<div class="header-top">
+    <div class="container">
+        <div class="row">
+            </div>
+			</div>
+		</div>
+
 <header id="main-header-section" class="sticky-banner">
     <div class="container">
         <div class="nav-header">
             <a href="#" class="js-main-nav-toggle main-nav-toggle dark"><i></i></a>
-            <h1 id="main-logo"><a href="index.php">Charity</a></h1>
+            <h1 id="main-logo"><a href="index.php">Animals Australia</a></h1>
             <nav id="main-menu-wrap" role="navigation">
                 <ul class="sf-menu" id="main-primary-menu">
-                    <li ><a href="index.php">Home</a></li>
-                    <li><a href="events.php">Events</a></li>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="events.php" class="main-sub-ddown">Events</a>
+                    </li>
+                    <li>
+                        <a href="volunteers.php" class="main-sub-ddown">Volunteers</a>
+                    </li>
                     <li><a href="about.php">About</a></li>
-                    <li><a href="blog.php">Blog</a></li>
+                    <li class="active"><a href="blog.php">News</a></li>
                     <li><a href="contact.php">Contact</a></li>
                     <?php if ($isLoggedin): ?>
                         <li><a href="logout.php">SignOut</a></li>
