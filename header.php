a8bc21cb374ada44b3594acdfc9769333cb7209d
Solale90 <solaleh.rm@gmail.com>
2020-02-27 19:53:49 -0800

    Refactoring.

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <div class="header-top">
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-sm-6 text-left fh5co-link">
+            <div class="col-md-6 col-sm-6 text-left wmt-link">
                 <a href="#">FAQ</a>
                 <a href="#">Forum</a>
                 <a href="#">Contact</a>
             </div>
-            <div class="col-md-6 col-sm-6 text-right fh5co-social">
+            <div class="col-md-6 col-sm-6 text-right wmt-social">
                 <a href="#" class="grow"><i class="icon-facebook2"></i></a>
                 <a href="#" class="grow"><i class="icon-twitter2"></i></a>
                 <a href="#" class="grow"><i class="icon-instagram2"></i></a>
@@ -14,20 +14,19 @@
         </div>
     </div>
 </div>
-<header id="fh5co-header-section" class="sticky-banner">
+<header id="wmt-header-section" class="sticky-banner">
     <div class="container">
         <div class="nav-header">
-            <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
-            <h1 id="fh5co-logo"><a href="index.html">Charity</a></h1>
-            <!-- START #fh5co-menu-wrap -->
-            <nav id="fh5co-menu-wrap" role="navigation">
-                <ul class="sf-menu" id="fh5co-primary-menu">
+            <a href="#" class="js-wmt-nav-toggle wmt-nav-toggle dark"><i></i></a>
+            <h1 id="wmt-logo"><a href="index.html">Charity</a></h1>
+            <nav id="wmt-menu-wrap" role="navigation">
+                <ul class="sf-menu" id="wmt-primary-menu">
                     <li>
                         <a href="index.html">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="fh5co-sub-ddown">Get Involved</a>
-                        <ul class="fh5co-sub-menu">
+                        <a href="#" class="wmt-sub-ddown">Get Involved</a>
+                        <ul class="wmt-sub-menu">
                             <li><a href="#">Donate</a></li>
                             <li><a href="#">Fundraise</a></li>
                             <li><a href="#">Campaign</a></li>
@@ -36,8 +35,8 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#" class="fh5co-sub-ddown">Projects</a>
-                            <ul class="fh5co-sub-menu">
+                        <a href="#" class="wmt-sub-ddown">Projects</a>
+                            <ul class="wmt-sub-menu">
                             <li><a href="#">Water World</a></li>
                             <li><a href="#">Cloth Giving</a></li>
                             <li><a href="#">Medical Mission</a></li>
